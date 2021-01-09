7fc2fd82aab76d00ca2ff201cf8352ee4d5dc45f
Szymon <Szymon>
2021-01-09 12:47:04 +0100

    Fixed

--- public/views/bookstore.php
+++ public/views/bookstore.php
@@ -9,16 +9,7 @@
 
 <body>
     <div class="logo_buttons">
-        <div class=title>
-            Book<br>Dynasty
-        </div>
-        <?php if(!isset($_COOKIE['name']) or !isset($_COOKIE['id'])):?>
-            <a href="login" class="login_button"><button >Log in <i class="material-icons my">person</i></button></a>
-            <a href="register" class="sign_up_button"><button>Sign up <i class="material-icons my" >person_add</i></button></a>
-        <?php else: ?>
-            <a href="profileInfo" class="login_button"><button><?= $_COOKIE['name']; ?> <i class="material-icons my" >person</i></button></a>
-            <a href="logout" class="sign_up_button"><button>Log out <i class="material-icons my" >exit_to_app</i></button></a>
-        <?php endif; ?>
+        <?php include ('header.php'); ?>
     </div>
     <div class="searchbar">
         <form method="POST" action="pass_argument">
--- public/views/search_result.php
+++ public/views/search_result.php
@@ -7,16 +7,7 @@
 </head>
 <body>
     <div class="logo_buttons">
-        <div class=title>
-            Book<br>Dynasty
-        </div>
-        <?php if(!isset($_COOKIE['name']) or !isset($_COOKIE['id'])):?>
-            <a href="login" class="login_button"><button >Log in <i class="material-icons my">person</i></button></a>
-            <a href="register" class="sign_up_button"><button>Sign up <i class="material-icons my" >person_add</i></button></a>
-        <?php else: ?>
-            <a href="profileInfo" class="login_button"><button><?= $_COOKIE['name']; ?> <i class="material-icons my" >person</i></button></a>
-            <a href="logout" class="sign_up_button"><button>Log out <i class="material-icons my" >exit_to_app</i></button></a>
-        <?php endif; ?>
+        <?php include ('header.php');?>
     </div>
     <div class="searchbar">
             <input placeholder="Search" name="search">
